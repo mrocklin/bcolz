6763443c717125f6ab0bb2a21f2aeca12bd1ff5d
Matthew Rocklin <mrocklin@gmail.com>
2014-11-25 08:27:50 -0800

    echo git describe
    binstar testing

--- conda.recipe/build.sh
+++ conda.recipe/build.sh
@@ -7,6 +7,7 @@ SRC_DIR=$RECIPE_DIR/..
 pushd $SRC_DIR
 
 # X.X.X.dev builds
+echo Version: $(git describe --tags)
 u_version=`git describe --tags | $PYTHON $SRC_DIR/conda.recipe/version.py`
 
 echo $u_version > __conda_version__.txt
