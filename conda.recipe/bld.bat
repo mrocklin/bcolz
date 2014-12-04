137baf05c883f4a086a2da5a95c4304eaecc3aa5
Matthew Rocklin <mrocklin@gmail.com>
2014-12-04 15:13:30 -0800

    use binstar bld.bat

--- conda.recipe/bld.bat
+++ conda.recipe/bld.bat
@@ -1,7 +1,3 @@
-@echo off
-
-SET BLD_DIR=%CD%
-cd /D "%RECIPE_DIR%\.."
-FOR /F "delims=" %%i IN ('git describe --tags') DO set BLAZE_VERSION=%%i
-echo.%BLAZE_VERSION% | %PYTHON% .\conda.recipe\version.py > %SRC_DIR%\__conda_version__.txt
-%PYTHON% setup.py --quiet install
+cd %RECIPE_DIR%\..
+%PYTHON% make_version.py
+%PYTHON% setup.py install
