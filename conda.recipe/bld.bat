97e82335424880ba236a1279f4a9e4969b606b1b
Matthew Rocklin <mrocklin@gmail.com>
2014-12-17 09:44:27 -0800

    another take at conda.recipe

--- conda.recipe/bld.bat
+++ conda.recipe/bld.bat
@@ -1,3 +1,7 @@
-cd %RECIPE_DIR%\..
-%PYTHON% make_version.py
-%PYTHON% setup.py install
+@echo off
+
+SET BLD_DIR=%CD%
+cd /D "%RECIPE_DIR%\.."
+FOR /F "delims=" %%i IN ('git describe --tags') DO set BCOLZ_VERSION=%%i
+echo.%BCOLZ_VERSION% | %PYTHON% .\conda.recipe\version.py > %SRC_DIR%\__conda_version__.txt
+%PYTHON% setup.py --quiet install
